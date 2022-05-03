7c68d39329de3776ef678413cafe397916722d6a
Vidyashree N M <vidyashree2109@gmail.com>
2022-05-04 00:26:17 +0530

    [Vidya] Add : Creation Of Stack Is Done

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace StackAndQueue;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("                                 WELCOME TO STACK AND QUEUE PROGRAM                                 ");
+        StackOperation stack = new StackOperation();
+        bool check = true;
+        Console.WriteLine("1.Stack Creation\n2.Exit");
+        while (check)
+        {
+            Console.WriteLine("\nEnter an option to execute: ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    stack.Push(56);
+                    stack.Push(30);
+                    stack.Push(70);
+                    stack.Display();
+                    break;
+                case 2:
+                    check = false;
+                    break;
+            }
+        }
+    }
+}
---
+++ StackAndQueue/StackOperation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class StackOperation
+    {
+        private Node top;
+        public StackOperation()
+        {
+            this.top = null;
+        }
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", value);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            Console.Write("\nStack -> ");
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
